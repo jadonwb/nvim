b1cb43fd12633e5dbb8d849e3deb95bc76df36bf
Jadon Brutcher <jadonwbrutcher@gmail.com>
2025-06-23 21:49:37 -0500

    added external app for special file support

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -16,3 +16,31 @@ vim.api.nvim_create_autocmd('BufEnter', {
   group = vim.api.nvim_create_augroup('disable_newline_comment', { clear = true }),
   desc = 'Disable New Line Comment',
 })
+
+-- [[ Open files with external applications ]]
+local function open_with_app(app)
+  return function()
+    if vim.fn.executable(app) == 0 then
+      return
+    end
+
+    local file_path = vim.fn.expand '%:p'
+    vim.fn.jobstart({ app, file_path }, { detach = true })
+    vim.cmd 'bdelete'
+  end
+end
+
+local file_associations = {
+  pdf = 'zathura', -- or "tdf"
+  -- You can add more file types here
+  -- png = "feh",
+  -- mp4 = "mpv",
+}
+
+for ext, app in pairs(file_associations) do
+  -- vim.api.nvim_create_autocmd({ 'BufReadPre', 'FileReadPre' }, {
+  vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    pattern = '*.' .. ext,
+    callback = open_with_app(app),
+  })
+end
--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -12,28 +12,4 @@ return {
       render_modes = { 'n', 'v' },
     },
   },
-  {
-    'toppair/peek.nvim',
-    event = { 'VeryLazy' },
-    build = 'deno task --quiet build:fast',
-    enabled = vim.fn.executable 'deno' == 1,
-    opts = function()
-      if vim.fn.getenv 'REMOTE_CONTAINERS' == 'true' then
-        return { app = 'browser' }
-      end
-      return {}
-    end,
-    init = function()
-      local peek = require 'peek'
-      vim.api.nvim_create_user_command('PeekOpen', peek.open, {})
-      vim.api.nvim_create_user_command('PeekClose', peek.close, {})
-      vim.api.nvim_create_user_command('Peek', function()
-        if peek.is_open() then
-          peek.close()
-          return
-        end
-        peek.open()
-      end, {})
-    end,
-  },
 }
