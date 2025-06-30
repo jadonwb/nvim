5245475d234caf076734bf9f35f746002ef36d11
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2025-06-30 14:36:03 -0500

    remote dev support

---
+++ lua/kickstart/plugins/remote.lua
@@ -0,0 +1,20 @@
+return {
+	"nosduco/remote-sshfs.nvim",
+	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		-- Refer to the configuration section below
+		-- or leave empty for defaults
+	},
+}
+
+-- return {
+-- 	"amitds1997/remote-nvim.nvim",
+-- 	version = "*",         -- Pin to GitHub releases
+-- 	dependencies = {
+-- 		"nvim-lua/plenary.nvim", -- For standard functions
+-- 		"MunifTanjim/nui.nvim", -- To build the plugin UI
+-- 		-- "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+-- 		"folke/snacks.nvim"
+-- 	},
+-- 	config = true,
+-- }
