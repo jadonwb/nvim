d9148d7126d76d93852ecbeac11a138fcd0310bd
Jadon Brutcher <jadonwbrutcher@gmail.com>
2025-07-16 19:25:03 -0500

    hex dump update

--- lua/kickstart/plugins/hex.lua
+++ lua/kickstart/plugins/hex.lua
@@ -1,22 +1,12 @@
-local function shell_command(file)
-  vim.fn.system(string.format("command -v '%s'", file))
-  if vim.v.shell_error ~= 0 then
-    return false
-  else
-    return true
-  end
-end
-
 local M = {
   'RaafatTurki/hex.nvim',
-  enabled = shell_command 'xxd',
   event = 'VeryLazy',
 }
 
 function M.init()
   local keymap = vim.keymap.set
 
-  keymap('n', '<leader>H', function()
+  keymap('n', '<leader>uh', function()
     require('hex').toggle()
   end, { desc = 'Toggle hex editor' })
 end
