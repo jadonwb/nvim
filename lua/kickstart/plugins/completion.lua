ac73386c26e537c56b9397803052a87ca242034c
Jadon Brutcher <jadonwbrutcher@gmail.com>
2025-07-17 21:27:24 -0500

    finally figured it out!

--- lua/kickstart/plugins/completion.lua
+++ lua/kickstart/plugins/completion.lua
@@ -1,3 +1,6 @@
+local supermaven_enabled = false
+local providers = { 'lsp', 'path', 'buffer', 'snippets', 'lazydev', 'spell' }
+
 return {
   'saghen/blink.cmp',
   event = { 'InsertEnter', 'CmdlineEnter' },
@@ -49,68 +52,69 @@ return {
     { 'xzbdmw/colorful-menu.nvim', opts = {} },
   },
   version = '*',
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    completion = {
-      list = {
-        selection = {
-          preselect = function(ctx)
-            return not require('blink.cmp').snippet_active { direction = 1 }
-          end,
-          auto_insert = false,
-        },
+  config = function()
+    vim.g.completion = true
+    vim.b.completion = vim.g.completion
+    require('which-key').add {
+      {
+        '<leader>uC',
+        function()
+          vim.g.completion = not (vim.g.completion or false)
+          vim.b.completion = vim.g.completion
+          print('Completion is ' .. (vim.b.completion and 'Enabled' or 'Disabled'))
+        end,
+        desc = function()
+          return (vim.b.completion and 'Disable' or 'Enable') .. ' Completion'
+        end,
+        icon = function()
+          return {
+            icon = vim.b.completion and '' or '',
+            color = vim.b.completion and 'green' or 'yellow',
+          }
+        end,
       },
-      menu = {
-        draw = {
-          columns = { { 'kind_icon' }, { 'label', gap = 2 } },
-          components = {
-            label = {
-              text = function(ctx)
-                return require('colorful-menu').blink_components_text(ctx)
-              end,
-              highlight = function(ctx)
-                return require('colorful-menu').blink_components_highlight(ctx)
-              end,
-            },
+    }
+    require('blink.cmp').setup {
+      enabled = function()
+        return vim.b.completion
+      end,
+      completion = {
+        list = {
+          selection = {
+            preselect = function(ctx)
+              return not require('blink.cmp').snippet_active { direction = 1 }
+            end,
+            auto_insert = false,
           },
         },
-        border = 'rounded',
-      },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 150,
-        window = {
+        menu = {
+          draw = {
+            columns = { { 'kind_icon' }, { 'label', gap = 2 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require('colorful-menu').blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require('colorful-menu').blink_components_highlight(ctx)
+                end,
+              },
+            },
+          },
           border = 'rounded',
-          max_width = 120,
-          max_height = 120,
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 150,
+          window = {
+            border = 'rounded',
+            max_width = 120,
+            max_height = 120,
+          },
         },
       },
-    },
-    keymap = {
-      preset = 'super-tab',
-      ['<CR>'] = {
-        function(cmp)
-          if cmp.is_menu_visible() then
-            cmp.select_and_accept()
-            return true
-          end
-        end,
-        'fallback',
-      },
-      ['<C-space>'] = {
-        'show',
-        'hide',
-      },
-      ['<C-k>'] = { 'show_documentation', 'hide_documentation' },
-      ['<Up>'] = {},
-      ['<Down>'] = {},
-    },
-    cmdline = {
       keymap = {
-        preset = 'none',
-        ['<Tab>'] = { 'show', 'select_next' },
-        ['<S-Tab>'] = { 'show', 'select_prev' },
+        preset = 'super-tab',
         ['<CR>'] = {
           function(cmp)
             if cmp.is_menu_visible() then
@@ -120,115 +124,125 @@ return {
           end,
           'fallback',
         },
-        ['<C-space>'] = { 'show', 'hide' },
-        ['<C-e>'] = {
+        ['<C-space>'] = {
           function(cmp)
-            if cmp.is_ghost_text_visible() then
-              return cmp.accept()
+            supermaven_enabled = not supermaven_enabled
+            if supermaven_enabled then
+              table.insert(providers, 'supermaven')
+              cmp.show { providers = providers }
+            else
+              table.remove(providers, #providers)
+              cmp.show { providers = providers }
             end
           end,
-          'fallback',
         },
+        ['<C-k>'] = { 'show_documentation', 'hide_documentation' },
+        ['<Up>'] = {},
+        ['<Down>'] = {},
       },
-      completion = {
-        list = {
-          selection = {
-            auto_insert = false,
+      cmdline = {
+        keymap = {
+          preset = 'none',
+          ['<Tab>'] = { 'show', 'select_next' },
+          ['<S-Tab>'] = { 'show', 'select_prev' },
+          ['<CR>'] = {
+            function(cmp)
+              if cmp.is_menu_visible() then
+                cmp.select_and_accept()
+                return true
+              end
+            end,
+            'fallback',
+          },
+          ['<C-space>'] = { 'show', 'hide' },
+          ['<C-e>'] = {
+            function(cmp)
+              if cmp.is_ghost_text_visible() then
+                return cmp.accept()
+              end
+            end,
+            'fallback',
           },
         },
-        menu = {
-          auto_show = false,
+        completion = {
+          list = {
+            selection = {
+              auto_insert = false,
+            },
+          },
+          menu = {
+            auto_show = false,
+          },
         },
       },
-    },
-    snippets = { preset = 'luasnip' },
-    sources = {
-      default = { 'supermaven', 'lsp', 'path', 'buffer', 'snippets', 'lazydev', 'spell' },
-      providers = {
-        supermaven = {
-          name = 'supermaven',
-          module = 'blink-cmp-supermaven',
-          async = true,
-          score_offset = 10,
-          enabled = true,
-          transform_items = function(ctx, items)
-            for _, item in ipairs(items) do
-              item.kind_icon = ''
-              item.kind_name = 'Supermaven'
-            end
-            return items
-          end,
-        },
-        snippets = {
-          module = 'blink.cmp.sources.snippets',
-          score_offset = -1,
-        },
-        lazydev = {
-          name = 'LazyDev',
-          module = 'lazydev.integrations.blink',
-          score_offset = 100,
-        },
-        spell = {
-          name = 'Spell',
-          module = 'blink-cmp-spell',
-          opts = {
-            -- Only enable source in `@spell` captures, and disable it
-            -- in `@nospell` captures.
-            enable_in_context = function()
-              local curpos = vim.api.nvim_win_get_cursor(0)
-              local captures = vim.treesitter.get_captures_at_pos(0, curpos[1] - 1, curpos[2] - 1)
-              local in_spell_capture = false
-              for _, cap in ipairs(captures) do
-                if cap.capture == 'spell' then
-                  in_spell_capture = true
-                elseif cap.capture == 'nospell' then
-                  return false
-                end
+      snippets = { preset = 'luasnip' },
+      sources = {
+        default = providers,
+        providers = {
+          supermaven = {
+            name = 'supermaven',
+            module = 'blink-cmp-supermaven',
+            enabled = function()
+              return supermaven_enabled
+            end,
+            async = true,
+            score_offset = 10,
+            transform_items = function(ctx, items)
+              for _, item in ipairs(items) do
+                item.kind_icon = ''
+                item.kind_name = 'Supermaven'
               end
-              return in_spell_capture
+              return items
             end,
           },
-        },
-      },
-    },
-    fuzzy = {
-      implementation = 'rust',
-      sorts = {
-        'exact',
-        'score',
-        'sort_text',
-      },
-      prebuilt_binaries = {
-        download = true,
-      },
-    },
-    appearance = {
-      nerd_font_variant = 'mono',
-      kind_icons = require('kickstart.icons').kind_icons,
-    },
-  },
-  init = function()
-    -- [[ Toggle completion ]]
-    local function toggle_completion()
-      local enabled = vim.b.completion
-      require('which-key').add {
-        {
-          '<leader>uC',
-          function()
-            vim.b.completion = not vim.b.completion
-            print('Completion is ' .. (enabled and 'Disabled' or 'Enabled'))
-            toggle_completion()
-          end,
-          desc = (enabled and 'Disable' or 'Enable') .. ' Completion',
-          icon = {
-            icon = enabled and '' or '',
-            color = enabled and 'green' or 'yellow',
+          snippets = {
+            module = 'blink.cmp.sources.snippets',
+            score_offset = -1,
+          },
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
+          spell = {
+            name = 'Spell',
+            module = 'blink-cmp-spell',
+            opts = {
+              -- Only enable source in `@spell` captures, and disable it
+              -- in `@nospell` captures.
+              enable_in_context = function()
+                local curpos = vim.api.nvim_win_get_cursor(0)
+                local captures = vim.treesitter.get_captures_at_pos(0, curpos[1] - 1, curpos[2] - 1)
+                local in_spell_capture = false
+                for _, cap in ipairs(captures) do
+                  if cap.capture == 'spell' then
+                    in_spell_capture = true
+                  elseif cap.capture == 'nospell' then
+                    return false
+                  end
+                end
+                return in_spell_capture
+              end,
+            },
           },
         },
-      }
-    end
-    -- Initialize the mapping for the first time
-    toggle_completion()
+      },
+      fuzzy = {
+        implementation = 'rust',
+        sorts = {
+          'exact',
+          'score',
+          'sort_text',
+        },
+        prebuilt_binaries = {
+          download = true,
+        },
+      },
+      appearance = {
+        nerd_font_variant = 'mono',
+        kind_icons = require('kickstart.icons').kind_icons,
+      },
+    }
   end,
 
   -- allows extending the providers array elsewhere in your config
