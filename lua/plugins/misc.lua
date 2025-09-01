9331cd1b4bb9eff62b3ce26613b25d58a63c4ec0
Jadon Brutcher <jadonwbrutcher@gmail.com>
2025-09-01 11:31:11 -0500

    remove unused stuff

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,16 +3,4 @@ return {
   { 'NMAC427/guess-indent.nvim', opts = {}, event = 'BufReadPre' },
   'tpope/vim-repeat', -- Make most of the plugins repeatable with .
   { 'anuvyklack/help-vsplit.nvim', opts = {} },
-  { 'yegappan/disassemble' },
-  {
-    'RaafatTurki/hex.nvim',
-    event = 'VeryLazy',
-    config = function()
-      local keymap = vim.keymap.set
-      keymap('n', '<leader>uH', function()
-        require('hex').toggle()
-      end, { desc = 'Toggle hex editor' })
-      require('hex').setup()
-    end,
-  },
 }
