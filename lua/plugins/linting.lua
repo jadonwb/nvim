e96ac9362a4a03c761d3dcaca914dcda6d90414b
Jadon Brutcher <jadonwbrutcher@gmail.com>
2025-09-18 20:40:13 -0500

    fix cmake lint?

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -6,4 +6,10 @@ return {
       bitbake = { 'oelint-adv' },
     },
   },
+  config = function(_, opts)
+    local lint = require 'lint'
+    lint.linters_by_ft = opts.linters_by_ft
+
+    lint.linters.cmakelint.args = { '--filter=-linelength' }
+  end,
 }
