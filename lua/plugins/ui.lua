1f85e67f21eea8eae449167c5a585d36e14e7ef5
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2025-09-25 09:50:42 -0500

    no longer need temp fix

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -66,14 +66,4 @@ return {
       },
     },
   },
-  -- temporary fix for catppuccin and layzvim
-  {
-    'akinsho/bufferline.nvim',
-    init = function()
-      local bufline = require 'catppuccin.groups.integrations.bufferline'
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
 }
