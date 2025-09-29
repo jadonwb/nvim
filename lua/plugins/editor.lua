e8bdcd808f05c97b03158213d83c5dd631578920
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2025-09-29 10:16:53 -0500

    fixed grug

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -15,4 +15,10 @@ return {
       },
     },
   },
+  {
+    'MagicDuck/grug-far.nvim',
+    keys = {
+      { '<localleader>', '<cmd>lua require("which-key").show("\\\\")<cr>', ft = 'grug-far' },
+    },
+  },
 }
