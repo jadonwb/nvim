499161880bde48a507decdc917ae45a73730fb85
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2025-10-02 15:27:22 -0500

    HUGE!! Copilot of by default

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,53 +1,85 @@
 return {
-  'folke/snacks.nvim',
-  opts = {
-    scroll = {
-      enabled = false,
+  {
+    'folke/snacks.nvim',
+    opts = {
+      scroll = {
+        enabled = false,
+      },
+      dashboard = {
+        enabled = false,
+      },
+      explorer = {
+        -- enabled = false,
+        replace_netrw = false,
+      },
     },
-    dashboard = {
-      enabled = false,
-    },
-    explorer = {
-      -- enabled = false,
-      replace_netrw = false,
+    keys = {
+      {
+        '<leader>,',
+        function()
+          Snacks.picker.buffers {
+            win = {
+              input = {
+                keys = {
+                  ['d'] = 'bufdelete',
+                  ['<c-d>'] = { 'bufdelete', mode = { 'n', 'i' } },
+                },
+              },
+              list = { keys = { ['d'] = 'bufdelete' } },
+            },
+          }
+        end,
+        desc = 'Buffers',
+      },
+      {
+        '<leader>n',
+        function()
+          Snacks.notifier.show_history()
+        end,
+        desc = 'Notification History',
+      },
+      {
+        '<leader>/',
+        function()
+          Snacks.picker.lines()
+        end,
+        desc = 'Buffer Lines',
+      },
+      {
+        '<leader>.',
+        LazyVim.pick 'grep',
+        desc = 'Grep (Root Dir)',
+      },
     },
   },
-  keys = {
-    {
-      '<leader>,',
-      function()
-        Snacks.picker.buffers {
-          win = {
-            input = {
-              keys = {
-                ['d'] = 'bufdelete',
-                ['<c-d>'] = { 'bufdelete', mode = { 'n', 'i' } },
-              },
-            },
-            list = { keys = { ['d'] = 'bufdelete' } },
-          },
-        }
-      end,
-      desc = 'Buffers',
-    },
-    {
-      '<leader>n',
-      function()
-        Snacks.notifier.show_history()
-      end,
-      desc = 'Notification History',
-    },
-    {
-      '<leader>/',
-      function()
-        Snacks.picker.lines()
-      end,
-      desc = 'Buffer Lines',
-    },
-    {
-      '<leader>.',
-      LazyVim.pick 'grep',
-      desc = 'Grep (Root Dir)',
-    },
+  {
+    'folke/snacks.nvim',
+    opts = function(_, opts)
+      local Snacks = require 'snacks'
+      local copilot_exists = pcall(require, 'copilot')
+
+      if copilot_exists then
+        vim.schedule(function()
+          local ok, _ = pcall(function()
+            vim.cmd 'messages clear'
+            require('copilot.command').disable()
+            vim.cmd 'messages clear'
+          end)
+        end)
+        Snacks.toggle({
+          name = 'Copilot Completion',
+          get = function()
+            return not require('copilot.client').is_disabled()
+          end,
+          set = function(state)
+            if state then
+              require('copilot.command').enable()
+            else
+              require('copilot.command').disable()
+            end
+          end,
+        }):map '<M-l>'
+      end
+    end,
   },
 }
