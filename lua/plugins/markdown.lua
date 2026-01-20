7e751be4baa5f5e5a1bc9e8027077fbfaa8c708c
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2026-01-20 11:58:53 -0600

    code block tab markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,53 +1,55 @@
 return {
-    {
-        "mfussenegger/nvim-lint",
-        opts = {
-            linters = {
-                ["markdownlint-cli2"] = {
-                    args = { "--config", vim.fn.expand("$HOME/.markdownlint-cli2.yaml"), "--" },
-                },
-            },
+  {
+    'mfussenegger/nvim-lint',
+    opts = {
+      linters = {
+        ['markdownlint-cli2'] = {
+          args = { '--config', vim.fn.expand '$HOME/.markdownlint-cli2.yaml', '--' },
         },
+      },
     },
-    {
-        "stevearc/conform.nvim",
-        opts = {
-            formatters = {
-                ["markdownlint-cli2"] = {
-                    args = { "--config", vim.fn.expand("$HOME/.markdownlint-cli2.yaml"), "--fix", "$FILENAME" },
-                },
-            },
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters = {
+        ['markdownlint-cli2'] = {
+          args = { '--config', vim.fn.expand '$HOME/.markdownlint-cli2.yaml', '--fix', '$FILENAME' },
         },
+      },
     },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-            heading = {
-                width = 'block',
-                min_width = 70,
-                border = true,
-                border_virtual = true,
-                icons = {
-                    "█" .. " " .. "󰉫" .. " ",
-                    "██" .. " " .. "󰉬" .. " ",
-                    "███" .. " " .. "󰉭" .. " ",
-                    "████" .. " " .. "󰉮" .. " ",
-                    "█████" .. " " .. "󰉯" .. " ",
-                    "██████" .. " " .. "󰉰" .. " ",
-                },
-            },
-            pipe_table = {
-                preset = 'heavy',
-            },
-            code = {
-                border = 'thick',
-                position = 'right',
-                language_right = '',
-                left_pad = 1,
-            },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      heading = {
+        width = 'block',
+        min_width = 70,
+        border = true,
+        border_virtual = true,
+        icons = {
+          '█' .. ' ' .. '󰉫' .. ' ',
+          '██' .. ' ' .. '󰉬' .. ' ',
+          '███' .. ' ' .. '󰉭' .. ' ',
+          '████' .. ' ' .. '󰉮' .. ' ',
+          '█████' .. ' ' .. '󰉯' .. ' ',
+          '██████' .. ' ' .. '󰉰' .. ' ',
         },
+      },
+      pipe_table = {
+        preset = 'heavy',
+      },
+      code = {
+        border = 'thick',
+        -- position = 'right',
+        language_left = '',
+        language_border = ' ',
+        language_right = '',
+        left_pad = 1,
+      },
     },
-    {
-        'jghauser/follow-md-links.nvim'
-    },
+  },
+  {
+    'jghauser/follow-md-links.nvim',
+  },
 }
