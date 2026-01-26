5ce252ae97a386a64997ef4d077ab8d47a92332a
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-01-25 19:51:07 -0600

    add back

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,7 @@ return {
         enabled = false,
       },
       dashboard = {
-        enabled = false,
+        enabled = true,
         preset = {
           keys = {
             { icon = ' ', key = 'e', desc = 'Yazi', action = ':Yazi' },
@@ -14,7 +14,6 @@ return {
             { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
             { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
             { icon = ' ', key = 's', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = ' ', key = 'l', desc = 'Restore Session', section = 'session' },
             { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
             { icon = '󰒲 ', key = 'E', desc = 'Extras', action = ':LazyExtras', enabled = package.loaded.lazy ~= nil },
             { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
