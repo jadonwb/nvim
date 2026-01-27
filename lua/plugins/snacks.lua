ab677d2d2de2ebfc7a8679de70a6b133f79d0991
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2026-01-27 11:42:53 -0600

    full size lazygit

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -65,6 +65,12 @@ return {
           },
         },
       },
+      styles = {
+        lazygit = {
+          width = 0,
+          height = 0,
+        },
+      },
     },
     keys = {
       {
