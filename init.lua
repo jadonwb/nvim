8eccf110c46ded6b4bdefd377e518271efaeb007
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-01-26 20:20:59 -0600

    smart session restore

--- init.lua
+++ init.lua
@@ -1,9 +1,37 @@
 require 'config.lazy'
 
-vim.api.nvim_create_autocmd('VimEnter', {
+local persistenceGroup = vim.api.nvim_create_augroup('Persistence', { clear = true })
+local home = vim.fn.expand '~'
+local disabled_dirs = {
+  home,
+  home .. '/Downloads',
+  '/tmp',
+  '/media',
+  '/mnt',
+}
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  group = persistenceGroup,
   callback = function()
-    if vim.fn.getcwd() ~= vim.env.HOME then
+    local cwd = vim.fn.getcwd()
+
+    for _, path in pairs(disabled_dirs) do
+      if path == cwd then
+        -- exact match
+        require('persistence').stop()
+        return
+      elseif path ~= home and vim.startswith(cwd, path .. '/') then
+        -- subdirectory match
+        require('persistence').stop()
+        return
+      end
+    end
+
+    if vim.fn.argc() == 0 and not vim.g.started_with_stdin then
+      vim.notify 'Restoring session...'
       require('persistence').load()
+    else
+      require('persistence').stop()
     end
   end,
   nested = true,
