af468c5b9e7ef2715370fc75533871a128f0d024
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2026-01-28 15:58:12 -0600

    add border slightly adjust location

--- lua/plugins/bento.lua
+++ lua/plugins/bento.lua
@@ -8,10 +8,10 @@ return {
       -- test
       floating = {
         position = 'bottom-right', -- See position options below
-        offset_x = 0, -- Horizontal offset from position
-        offset_y = -3, -- Vertical offset from position
+        offset_x = -5, -- Horizontal offset from position
+        offset_y = -4, -- Vertical offset from position
         dash_char = '─', -- Character for collapsed dashes
-        border = 'none', -- "rounded" | "single" | "double" | etc. (see :h winborder)
+        border = 'rounded', -- "rounded" | "single" | "double" | etc. (see :h winborder)
         label_padding = 1, -- Padding around labels
         minimal_menu = 'filename', -- nil | "dashed" | "filename" | "full"
         max_rendered_buffers = nil, -- nil (no limit) or number for pagination
