da6dce6d08636f5ea655fa706c76486672a9fc0a
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2026-02-12 17:21:13 -0600

    add args

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,6 +5,9 @@ return {
       ['markdownlint-cli2'] = {
         args = { '--config', vim.fn.expand '$HOME/.markdownlint-cli2.yaml', '--' },
       },
+      ['oelint-adv'] = {
+        args = { '--cached' },
+      },
     },
     linters_by_ft = {
       --   cmake = {},
