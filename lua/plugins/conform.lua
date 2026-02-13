cc00182ab41ff96eecddbe2f254e74148544648a
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2026-02-13 14:33:25 -0600

    try defaults

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,14 +6,10 @@ return {
       formatters_by_ft = {
         sh = { 'shfmt' },
         bash = { 'shfmt' },
+        zsh = { 'shfmt' },
         c = { 'clang-format' },
         cpp = { 'clang-format' },
         env = {},
-      },
-      formatters = {
-        shfmt = {
-          prepend_args = { '-i', '2', '-ci', '-sr', '-kp' },
-        },
       },
     },
   },
