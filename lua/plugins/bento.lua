c987998b4ddc98946b6bdd9ce6180966083f0020
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-05-09 14:59:47 -0500

    bye for now

--- lua/plugins/bento.lua
+++ lua/plugins/bento.lua
@@ -1,32 +1,32 @@
 return {
-  'jadonwb/bento.nvim',
-  branch = 'jadon',
-  opts = {
-    -- main_keymap = '_',
-    main_keymap = ';',
-    ordering_metric = 'access',
-    ui = {
-      mode = 'floating', -- "floating" | "tabline"
-      -- test
-      floating = {
-        alignment = 'left',
-        position = 'bottom-right', -- See position options below
-        offset_x = 0, -- Horizontal offset from position
-        offset_y = -2, -- Vertical offset from position
-        dash_char = '_', -- Character for collapsed dashes
-        border = nil, -- "rounded" | "single" | "double" | etc. (see :h winborder)
-        label_padding = 1, -- Padding around labels
-        minimal_menu = 'dashed', -- nil | "dashed" | "filename" | "full"
-        max_rendered_buffers = nil, -- nil (no limit) or number for pagination
-      },
-    },
-    actions = {
-      split = {
-        key = '\\',
-      },
-    },
-    highlights = {
-      current = '@markup.link',
-    },
-  },
+  -- 'jadonwb/bento.nvim',
+  -- branch = 'jadon',
+  -- opts = {
+  --   -- main_keymap = '_',
+  --   main_keymap = ';',
+  --   ordering_metric = 'access',
+  --   ui = {
+  --     mode = 'floating', -- "floating" | "tabline"
+  --     -- test
+  --     floating = {
+  --       alignment = 'left',
+  --       position = 'bottom-right', -- See position options below
+  --       offset_x = 0, -- Horizontal offset from position
+  --       offset_y = -2, -- Vertical offset from position
+  --       dash_char = '_', -- Character for collapsed dashes
+  --       border = nil, -- "rounded" | "single" | "double" | etc. (see :h winborder)
+  --       label_padding = 1, -- Padding around labels
+  --       minimal_menu = 'dashed', -- nil | "dashed" | "filename" | "full"
+  --       max_rendered_buffers = nil, -- nil (no limit) or number for pagination
+  --     },
+  --   },
+  --   actions = {
+  --     split = {
+  --       key = '\\',
+  --     },
+  --   },
+  --   highlights = {
+  --     current = '@markup.link',
+  --   },
+  -- },
 }
