47337b7b58d93ad33b3c02b14ca36dcc5c63bc7b
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-06-12 10:46:42 -0500

    very good

--- after/ftplugin/bitbake.lua
+++ after/ftplugin/bitbake.lua
@@ -1,11 +1,6 @@
 vim.diagnostic.enable(false, { bufnr = 0 })
 
-local map = vim.keymap.set
-
-map('n', '<leader>mta', '<cmd>Table Align<cr>', { desc = 'Align Table' })
-map('n', '<leader>mtc', '<cmd>Table Complete<cr>', { desc = 'Complete Table' })
-
-map('n', '<leader>ud', function()
+vim.keymap.set('n', '<leader>ud', function()
   local enabled = vim.diagnostic.is_enabled({ bufnr = 0 })
   vim.diagnostic.enable(not enabled, { bufnr = 0 })
   local status = not enabled and 'Enabled' or 'Disabled'
