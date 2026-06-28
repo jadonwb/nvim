9e090e95a0cbbb62c1b82423143f14066ebe5734
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-06-28 12:50:16 -0500

    nice

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,18 +10,6 @@ return {
     },
   },
   {
-    'nvim-mini/mini.hipatterns',
-    opts = {
-      highlighters = {
-        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-        fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-        hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-        todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-        note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
-      },
-    },
-  },
-  {
     'nvim-mini/mini.pairs',
     event = 'VeryLazy',
     opts = {
