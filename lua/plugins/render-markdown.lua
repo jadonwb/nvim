6ee27675082c4091434899dc13bed95116ef320d
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-06 11:10:57 -0500

    test

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       heading = {
         width = 'block',
-        min_width = 100,
+        min_width = 85,
         border = true,
         border_virtual = true,
         icons = {
