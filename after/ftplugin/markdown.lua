f7a16d5b108713dd340c6633383559be25329788
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-06 10:53:04 -0500

    put back

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,13 @@
+vim.diagnostic.enable(false, { bufnr = 0 })
+
 local map = vim.keymap.set
 
 map('n', '<leader>mta', '<cmd>Table Align<cr>', { desc = 'Align Table' })
 map('n', '<leader>mtc', '<cmd>Table Complete<cr>', { desc = 'Complete Table' })
+
+map('n', '<leader>ud', function()
+  local enabled = vim.diagnostic.is_enabled { bufnr = 0 }
+  vim.diagnostic.enable(not enabled, { bufnr = 0 })
+  local status = not enabled and 'Enabled' or 'Disabled'
+  vim.notify(status .. ' Diagnostics', not enabled and vim.log.levels.INFO or vim.log.levels.WARN, { title = 'Diagnostics' })
+end, { buffer = true, desc = 'Toggle Diagnostics' })
