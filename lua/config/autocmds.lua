70f210b112c86dbbe3908bb189801cb209792e29
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-06 11:10:02 -0500

    ok

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,9 +17,11 @@ vim.api.nvim_create_autocmd('BufReadPre', {
   end,
 })
 
+--override lazyvim's markdown settings
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'markdown',
+  pattern = { 'markdown' },
   callback = function()
-    vim.cmd 'NoNeckPain'
+    vim.opt_local.wrap = false
+    vim.opt_local.spell = false
   end,
 })
