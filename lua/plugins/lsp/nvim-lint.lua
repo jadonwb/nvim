56695cb8abd47ce916e1a0f36d75bcc00987d689
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-26 15:54:16 -0500

    cleanup

--- lua/plugins/lsp/nvim-lint.lua
+++ lua/plugins/lsp/nvim-lint.lua
@@ -5,9 +5,6 @@ return {
       ['markdownlint-cli2'] = {
         args = { '--config', vim.fn.expand '$HOME/.markdownlint-cli2.yaml', '--' },
       },
-      ['oelint-adv'] = {
-        args = { '--cached' },
-      },
     },
     linters_by_ft = {
       bitbake = { 'oelint-adv' },
