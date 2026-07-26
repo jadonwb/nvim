681b6a63efaaeaae46ae1e60bf06e0acc13617f5
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-26 15:54:23 -0500

    new autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -26,3 +26,13 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.spell = false
   end,
 })
+
+-- Auto-reload files when they change externally
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  callback = function()
+    if vim.api.nvim_get_option_value('buftype', { buf = 0 }) == '' then
+      vim.cmd 'checktime'
+    end
+  end,
+})
