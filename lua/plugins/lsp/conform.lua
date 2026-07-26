aeb538ef4b4cfcb8c95ae4c6cc9adbe521fad31b
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-26 16:17:25 -0500

    probably was wrong the whole time

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -8,20 +8,4 @@ return {
       },
     },
   },
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      formatters = {
-        ['markdownlint-cli2'] = {
-          -- condition = function(_, ctx)
-          --   local diag = vim.tbl_filter(function(d)
-          --     return d.source == 'markdownlint'
-          --   end, vim.diagnostic.get(ctx.buf))
-          --   return #diag > 0
-          -- end,
-          args = { '--config', vim.fn.expand '$HOME/.markdownlint-cli2.yaml', '--' },
-        },
-      },
-    },
-  },
 }
