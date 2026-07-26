2f6e3b210f58afd2804b4a5c0fd082d6468356f7
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-26 16:22:11 -0500

    fix treesitter

--- lua/plugins/lsp/nvim-treesitter.lua
+++ lua/plugins/lsp/nvim-treesitter.lua
@@ -1,18 +1,7 @@
 return {
   {
-    'Wansmer/treesj',
-    keys = {
-      {
-        '<leader>cj',
-        '<cmd>TSJToggle<cr>',
-        mode = 'n',
-        desc = 'Join or Split Code Block',
-      },
-    },
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    'nvim-treesitter/nvim-treesitter',
     opts = {
-      use_default_keymaps = false,
-      max_join_length = 168,
       ensure_installed = {
         'devicetree',
         'lua',
@@ -34,6 +23,46 @@ return {
         'html',
         'css',
         'glsl',
+      },
+    },
+  },
+  {
+    'Wansmer/treesj',
+    keys = {
+      {
+        '<leader>cj',
+        '<cmd>TSJToggle<cr>',
+        mode = 'n',
+        desc = 'Join or Split Code Block',
+      },
+    },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      use_default_keymaps = false,
+      max_join_length = 168,
+    },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'LazyFile',
+    opts = {
+      enable = false,
+      mode = 'cursor',
+      max_lines = 3,
+    },
+    keys = {
+      {
+        '<leader>ut',
+        function()
+          local tsc = require 'treesitter-context'
+          tsc.toggle()
+          if LazyVim.inject.get_upvalue(tsc.toggle, 'enabled') then
+            LazyVim.info('Enabled Treesitter Context', { title = 'Option' })
+          else
+            LazyVim.warn('Disabled Treesitter Context', { title = 'Option' })
+          end
+        end,
+        desc = 'Toggle Treesitter Context',
       },
     },
   },
