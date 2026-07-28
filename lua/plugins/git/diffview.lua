1a6c4b66177b648338dd8185fe046476e06fd4a3
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-28 11:12:32 -0500

    more explicit keymaps

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -33,12 +33,13 @@ return {
       -- File panel (the tree listing on the left)
       -- stylua: ignore
       file_panel = {
+        { 'n', '<Tab>', function() require('diffview.actions').select_next_entry() end, { desc = 'Next file' } },
+        { 'n', '<S-Tab>', function() require('diffview.actions').select_prev_entry() end, { desc = 'Previous file' } },
         { 'n', 'j', function() require('diffview.actions').next_entry() end, { desc = 'Next entry' } },
         { 'n', 'k', function() require('diffview.actions').prev_entry() end, { desc = 'Previous entry' } },
         { 'n', '<Down>', function() require('diffview.actions').next_entry() end },
         { 'n', '<Up>', function() require('diffview.actions').prev_entry() end },
-        { 'n', '<Left>', function() require('diffview.actions').select_entry() end, { desc = 'Open diff for entry' } },
-        { 'n', '<Right>', function() require('diffview.actions').select_entry() end, { desc = 'Open diff for entry' } },
+
         { 'n', '<CR>', function() require('diffview.actions').select_entry() end, { desc = 'Show diff for entry' } },
         { 'n', '<C-CR>', function() require('diffview.actions').toggle_stage_entry() end, { desc = 'Stage / unstage entry' } },
         { 'n', 'A', function() require('diffview.actions').stage_all() end, { desc = 'Stage all' } },
@@ -48,6 +49,52 @@ return {
         { 'n', 'i', function() require('diffview.actions').listing_style() end, { desc = 'Toggle list / tree view' } },
         { 'n', '?', function() require('diffview.actions').help('file_panel') end, { desc = 'Open help' } },
         { 'n', 'q', function() vim.cmd 'DiffviewClose' end, { desc = 'Close Diffview' } },
+      },
+
+      -- File history panel (the bottom split)
+      -- stylua: ignore
+      file_history_panel = {
+        { 'n', '<Tab>', function() require('diffview.actions').select_next_entry() end, { desc = 'Next file' } },
+        { 'n', '<S-Tab>', function() require('diffview.actions').select_prev_entry() end, { desc = 'Previous file' } },
+        { 'n', 'q', function() require('diffview.actions').close() end, { desc = 'Close Diffview' } },
+      },
+
+      -- stylua: ignore
+      diff1 = {
+        -- Mappings in single window diff layouts
+        { 'n', 'g?', function() require('diffview.actions').help { 'view', 'diff1' } end, { desc = 'Open the help panel' } },
+      },
+      -- stylua: ignore
+      diff2 = {
+        -- Mappings in 2-way diff layouts
+        { 'n', 'g?', function() require('diffview.actions').help { 'view', 'diff2' } end, { desc = 'Open the help panel' } },
+      },
+      -- stylua: ignore
+      diff3 = {
+        -- Mappings in 3-way diff layouts
+        { { 'n', 'x' }, '2do', function() require('diffview.actions').diffget 'ours' end, { desc = 'Obtain the diff hunk from the OURS version of the file' } },
+        { { 'n', 'x' }, '3do', function() require('diffview.actions').diffget 'theirs' end, { desc = 'Obtain the diff hunk from the THEIRS version of the file' } },
+        { 'n', 'g?', function() require('diffview.actions').help { 'view', 'diff3' } end, { desc = 'Open the help panel' } },
+      },
+      -- stylua: ignore
+      diff4 = {
+        -- Mappings in 4-way diff layouts
+        { { 'n', 'x' }, '1do', function() require('diffview.actions').diffget 'base' end, { desc = 'Obtain the diff hunk from the BASE version of the file' } },
+        { { 'n', 'x' }, '2do', function() require('diffview.actions').diffget 'ours' end, { desc = 'Obtain the diff hunk from the OURS version of the file' } },
+        { { 'n', 'x' }, '3do', function() require('diffview.actions').diffget 'theirs' end, { desc = 'Obtain the diff hunk from the THEIRS version of the file' } },
+        { 'n', 'g?', function() require('diffview.actions').help { 'view', 'diff4' } end, { desc = 'Open the help panel' } },
+      },
+
+      -- stylua: ignore
+      option_panel = {
+        { 'n', '<tab>', function() require('diffview.actions').select_entry() end, { desc = 'Change the current option' } },
+        { 'n', 'q', function() require('diffview.actions').close() end, { desc = 'Close the panel' } },
+        { 'n', 'g?', function() require('diffview.actions').help 'option_panel' end, { desc = 'Open the help panel' } },
+      },
+      -- stylua: ignore
+      help_panel = {
+        { 'n', 'q', function() require('diffview.actions').close() end, { desc = 'Close help menu' } },
+        { 'n', '<esc>', function() require('diffview.actions').close() end, { desc = 'Close help menu' } },
       },
     },
 
