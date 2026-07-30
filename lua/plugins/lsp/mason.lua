a26ab9e5fc82bfc2fea73128e15b360862487879
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-30 14:03:27 -0500

    tweak

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -3,7 +3,8 @@ return {
   opts = {
     ui = {
       backdrop = 100,
-      height = 0.8,
+      width = 0.8,
+      height = 0.79, -- why tf are mason and lazy not centered the same, its a 1 row 1 col difference and its not configurable
     },
     ensure_installed = {
       'clang-format',
