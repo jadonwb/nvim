65904d1d3dcad52327e457ea623d561df172b003
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-30 14:13:43 -0500

    arrowlake does the highlights now

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -7,18 +7,6 @@ return {
       focused_win = true,
       only_win = false,
     },
-    highlight = {
-      groups = {
-        InclineNormal = {
-          default = true,
-          group = 'StatusLine',
-        },
-        InclineNormalNC = {
-          default = true,
-          group = 'StatusLineNC',
-        },
-      },
-    },
     render = function(props)
       local bufname = vim.api.nvim_buf_get_name(props.buf)
       if bufname == '' then
@@ -29,18 +17,10 @@ return {
       local icon, _ = require('nvim-web-devicons').get_icon(filename, ext)
       local modified = vim.bo[props.buf].modified
 
-      -- Resolve colors from theme highlight groups (updates on colorscheme change)
-      local bg_hl = vim.api.nvim_get_hl(0, { name = 'StatusLine', link = false })
-      local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment', link = false })
-      local bg = bg_hl.bg and string.format('#%06x', bg_hl.bg)
-      local fg = comment_hl.fg and string.format('#%06x', comment_hl.fg)
-
       return {
-        { (icon or '') .. ' ', guibg = bg, guifg = fg },
+        { (icon or '') .. ' ' },
         {
           filename .. ' ',
-          guibg = bg,
-          guifg = fg,
           gui = modified and 'bold,italic' or 'bold',
         },
       }
