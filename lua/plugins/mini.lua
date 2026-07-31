a7d0f07aee759f29f55b2e9b8d9599efe7cf15c2
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-30 23:09:26 -0500

    cleanup

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,22 +3,13 @@ return {
     'nvim-mini/mini.ai',
     event = 'VeryLazy',
     opts = {
-      custom_textobjects = {
-        v = require('mini.ai').gen_spec.treesitter { a = '@assignment.outer', i = '@assignment.lhs' },
-        V = require('mini.ai').gen_spec.treesitter { a = '@assignment.outer', i = '@assignment.rhs' },
-      },
+      silent = true,
     },
   },
-  -- FIXME: only remap s during visual mode? I prefer to highlight the textobject before I do surrounding keymap
-  -- {
-  --   'nvim-mini/mini.surround',
-  --   opts = {
-  --     silent = true,
-  --     mappings = {
-  --       add = 'sa',
-  --       replace = 'sc',
-  --       delete = 'sd',
-  --     },
-  --   },
-  -- },
+  {
+    'nvim-mini/mini.surround',
+    opts = {
+      silent = true,
+    },
+  },
 }
