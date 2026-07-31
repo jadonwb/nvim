86764effeb0205ee267da369ded9435085369012
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-30 21:36:37 -0500

    good

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
   { 'catppuccin', enabled = false },
   { 'folke/tokyonight.nvim', enabled = false },
   { 'akinsho/bufferline.nvim', enabled = false },
+  { 'nvim-mini/mini.pairs', enabled = false },
   {
     'LazyVim/LazyVim',
     opts = {
