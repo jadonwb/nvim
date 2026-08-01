fe60d7ae137f76d05d475b623df609cd8c0492d7
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-01 17:29:06 -0500

    cleanup

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -1,15 +1,3 @@
-local source_name = 'diagnostics_per_ft'
-
-local function open_trouble(filter, buf)
-  local trouble = require 'trouble'
-  trouble.open {
-    mode = source_name,
-    focus = true,
-    filter = vim.tbl_extend('keep', filter or {}, buf and { buf = buf } or {}),
-    win = { size = 0.4, position = 'bottom' },
-  }
-end
-
 return {
   {
     'folke/trouble.nvim',
@@ -43,69 +31,5 @@ return {
         ['<Space>'] = 'fold_toggle',
       })
     end,
-    config = function(_, opts)
-      require('trouble').setup(opts)
-
-      local ok = pcall(require, 'trouble.sources')
-      if not ok then
-        return
-      end
-
-      require('trouble.sources').register(source_name, {
-        get = function(cb)
-          local Item = require 'trouble.item'
-          local items = {}
-          for _, client in ipairs(vim.lsp.get_clients { bufnr = 0 }) do
-            local ns = vim.lsp.diagnostic.get_namespace(client.id)
-            for _, d in ipairs(vim.diagnostic.get(nil, { namespace = ns })) do
-              table.insert(
-                items,
-                Item.new {
-                  source = 'diagnostics',
-                  buf = d.bufnr,
-                  pos = { d.lnum + 1, d.col },
-                  end_pos = { d.end_lnum and (d.end_lnum + 1) or nil, d.end_col },
-                  item = d,
-                }
-              )
-            end
-          end
-          cb(items)
-        end,
-        config = {
-          format = '{severity_icon} {message:md} {item.source} {code} {pos}',
-          groups = {
-            { 'directory' },
-            { 'filename', format = '{file_icon} {basename} {count}' },
-          },
-          modes = {
-            [source_name] = { source = source_name },
-          },
-        },
-      })
-    end,
-    keys = {
-      {
-        '<leader>xde',
-        function()
-          open_trouble { severity = vim.diagnostic.severity.ERROR }
-        end,
-        desc = 'Workspace Errors (Trouble)',
-      },
-      {
-        '<leader>xdw',
-        function()
-          open_trouble { severity = vim.diagnostic.severity.WARN }
-        end,
-        desc = 'Workspace Warnings (Trouble)',
-      },
-      {
-        '<leader>xdd',
-        function()
-          open_trouble()
-        end,
-        desc = 'Workspace Diagnostics (Trouble)',
-      },
-    },
   },
 }
