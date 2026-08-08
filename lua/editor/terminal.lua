537eeb5f8713250db3c0f43bd9c57c2f80cd4727
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-07 20:29:38 -0500

    ok

--- lua/editor/terminal.lua
+++ lua/editor/terminal.lua
@@ -1,30 +1,28 @@
 NVTerminal = {}
 
--- TODO!: make sure I am happy with these?
--- TODO!: make this open a new tab specifically for terminal, it can be split as well, but only terminal.
--- detect cwd for git-worktree, to make terminal for it?
--- TODO!: also make an option to open it as a vsplit term
+local fn = {}
+
+-- TODO!: make this open a new tab specifically for terminal, it can be split as well
+-- TODO!: also make it open as a vsplit term
 
 function NVTerminal.keymaps()
-  K.map {
-    '<C-v>',
-    'Paste text',
-    function()
-      local content = vim.fn.getreg '+'
-      content = vim.api.nvim_replace_termcodes(content, true, true, true)
-      vim.api.nvim_feedkeys(content, 't', true)
-    end,
-    mode = 't',
-  }
-  K.map { '<C-Up>', 'Exit terminal mode', '<C-\\><C-n>', mode = 't' }
+  K.map { '<C-v>', 'Paste text', fn.paste, mode = 't', expr = true }
+  K.map { NVKeymaps.scroll.up, 'Exit terminal mode', '<C-\\><C-n>', mode = 't' }
+  K.map { NVKeymaps.scroll_alt.up, 'Exit terminal mode', '<C-\\><C-n>', mode = 't' }
+  K.map { NVKeymaps.scroll_ctx.up, 'Lazygit: Scroll up main panel', '<C-\\><C-u>', mode = 't' }
+  K.map { NVKeymaps.scroll_ctx.down, 'Lazygit: Scroll down main panel', '<C-\\><C-d>', mode = 't' }
 
   vim.api.nvim_create_autocmd('FileType', {
     pattern = 'snacks_terminal',
     callback = function()
       K.map { '&', 'Enter terminal mode', 'i', mode = 'n', buffer = true }
       K.map { '&', 'Enter terminal mode', '<Esc>i', mode = 'v', buffer = true }
-      K.map { '<C-S-Up>', 'Lazygit: Scroll up', '<C-\\><C-u>', mode = 't', buffer = true }
-      K.map { '<C-S-Down>', 'Lazygit: Scroll down', '<C-\\><C-d>', mode = 't', buffer = true }
     end,
   })
+end
+
+function fn.paste()
+  local content = vim.fn.getreg '*'
+  content = vim.api.nvim_replace_termcodes(content, true, true, true)
+  vim.api.nvim_feedkeys(content, 't', true)
 end
