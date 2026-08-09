f2a9b8720c43138cd878f3730a80f86e6124db9a
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-08 22:39:11 -0500

    good

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,55 +1,53 @@
--- TODO!: make esc cancel/hide the completion menu like how we do hover and diagnostic popups
--- FIXME!: how to configure how wide the definition/preview window is? I want to make it generally a bit less wide and have it scroll if needed?
--- TODO!: make shift-enter be newline when incompletion, make it preselect, make enter be confirm, tab/shift-tab for snippet thingy
--- TODO?: what does the new blink branch have to offer? will it break other plugins?
-return {
-  {
-    'saghen/blink.cmp',
-    dependencies = {
-      { 'xzbdmw/colorful-menu.nvim', opts = {} },
+NVBlink = {
+  'saghen/blink.cmp',
+  dependencies = {
+    { 'xzbdmw/colorful-menu.nvim', opts = {} },
+  },
+  opts = {
+    sources = {
+      per_filetype = {
+        ['pi-chat-prompt'] = { 'pi' },
+      },
+      providers = {
+        pi = { name = 'Pi', module = 'pi.completion.blink' },
+      },
     },
-    opts = {
-      sources = {
-        per_filetype = {
-          ['pi-chat-prompt'] = { 'pi' },
-        },
-        providers = {
-          pi = { name = 'Pi', module = 'pi.completion.blink' },
+    completion = {
+      list = {
+        selection = {
+          preselect = true,
         },
       },
-      completion = {
-        list = {
-          selection = {
-            preselect = false,
-          },
+      documentation = {
+        auto_show = false,
+        window = {
+          border = NVBorders.padded,
         },
-        documentation = {
-          auto_show = true,
-          window = {
-            border = NVBorders.padded,
-          },
-        },
-        -- ghost_text = { enabled = false },
-        menu = {
-          border = NVBorders.completion,
-          draw = {
-            components = {
-              label = {
-                text = function(ctx)
-                  return require('colorful-menu').blink_components_text(ctx)
-                end,
-                highlight = function(ctx)
-                  return require('colorful-menu').blink_components_highlight(ctx)
-                end,
-              },
+      },
+      -- ghost_text = { enabled = false },
+      menu = {
+        border = NVBorders.completion,
+        draw = {
+          -- We don't need label_description now because label and label_description are already
+          -- combined together in label by colorful-menu.nvim.
+          columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require('colorful-menu').blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require('colorful-menu').blink_components_highlight(ctx)
+              end,
             },
           },
         },
       },
-      keymap = {
-        preset = 'enter',
-        [NVKeymaps.close_esc] = { 'hide', 'fallback' },
-      },
+    },
+    keymap = {
+      preset = 'enter',
     },
   },
 }
+
+return { NVBlink }
