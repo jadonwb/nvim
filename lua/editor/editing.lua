fc3987398aaef336de4e69c768a767397e56d673
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-09 01:45:37 -0500

    good

--- lua/editor/editing.lua
+++ lua/editor/editing.lua
@@ -101,9 +101,6 @@ function NVEditing.keymaps()
   K.map { 'J', 'Join lines and keep cursor position', 'mzJ`z', mode = 'n' }
   K.map { 'U', 'Redo', '<C-r>', mode = 'n' }
 
-  K.map { '<M-o>', 'New line below', 'o<Esc>', mode = 'n' }
-  K.map { '<M-S-o>', 'New line above', 'O<Esc>', mode = 'n' }
-
   K.map { '<C-S-c>', 'Copy selected text', [["+y]], mode = 'v' } -- TODO: make into omarchy universal copy Ctrl+Insert
   K.map { '<C-S-v>', 'Paste text', 'P', mode = { 'n', 'v' } } -- TODO: make into omarchy universal paste Shift+Insert
   K.map { '<C-S-v>', 'Paste text', fn.paste, mode = { 'i', 'c' } }
@@ -125,6 +122,28 @@ function NVEditing.keymaps()
   K.map { 's', "Don't replace clipboard content when inserting", [["xs]], mode = 'v' }
   K.map { 'c', "Don't replace clipboard content when changing", [["xc]], mode = { 'n', 'v' } }
 
+  K.map { '<A-CR>', 'Insert new line above', 'O<Esc>', mode = 'n' }
+  K.map { '<S-CR>', 'Insert new line below', 'o<Esc>', mode = 'n' }
+  K.map { '<A-CR>', 'Insert new line above', '<Esc>O', mode = 'i' }
+  K.map { '<S-CR>', 'Insert new line below', '<Esc>o', mode = 'i' }
+
+  K.map { '<A-Up>', 'Move line up', '<Cmd>m .-2<CR>==', mode = 'n' }
+  K.map { '<A-Down>', 'Move line down', '<Cmd>m .+1<CR>==', mode = 'n' }
+  K.map { '<A-Up>', 'Move line up', '<Esc><Cmd>m .-2<CR>==gi', mode = 'i' }
+  K.map { '<A-Down>', 'Move line down', '<Esc><Cmd>m .+1<CR>==gi', mode = 'i' }
+  K.map { '<A-Up>', 'Move selected lines up', ":m '<-2<CR>gv=gv", mode = 'v' }
+  K.map { '<A-Down>', 'Move selected lines down', ":m '>+1<CR>gv=gv", mode = 'v' }
+
+  K.map { '<Tab>', 'Indent', '>>', mode = 'n' }
+  K.map { '<S-Tab>', 'Unindent', '<<', mode = 'n' }
+  K.map { '<Tab>', 'Indent', '>gv', mode = 'v' }
+  K.map { '<S-Tab>', 'Unindent', '<gv', mode = 'v' }
+
+  K.map { '<A-Left>', 'Jump one word to the left', "<C-o><Cmd>lua require('spider').motion('b')<CR>", mode = 'i' }
+  K.map { '<A-Right>', 'Jump one word to the right', fn.jump_to_end_of_word, mode = 'i' }
+  -- K.map { '<A-Left>', 'Jump to the beginning of the line', '<C-o>I', mode = 'i' }
+  -- K.map { '<A-Right>', 'Jump to the end of the line', '<C-o>A', mode = 'i' }
+
   -- TODO: make new Alt-q, Alt-Q, etc. keymaps for closing and restarting and stuff
   -- make an option to exit and wipe session?
   K.map {
@@ -140,6 +159,38 @@ function NVEditing.keymaps()
   }
 
   K.map { '<leader>u<tab>', 'Toggle tab characters', fn.toggle_tabs, mode = 'n' }
+
+  vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*',
+    callback = function()
+      if vim.bo.filetype ~= 'snacks_input' and vim.bo.filetype ~= 'snacks_picker_input' and vim.bo.filetype ~= 'delta-input' then
+        K.map { '<M-BS>', 'Delete word to the left', '<C-w>', mode = 'i', buffer = true }
+      end
+    end,
+  })
+  vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = { 'snacks_input', 'snacks_picker_input', 'delta-input' },
+    callback = function()
+      K.map { '<M-BS>', 'Delete word to the left', '<C-S-w>', mode = 'i', buffer = true }
+    end,
+  })
+  vim.api.nvim_create_autocmd('CmdlineEnter', {
+    pattern = '*',
+    callback = function()
+      K.map {
+        '<M-BS>',
+        'Delete word to the left',
+        function()
+          NVKeys.send('<C-w>', { mode = 'n' })
+          vim.schedule(function()
+            vim.cmd 'redraw'
+          end)
+        end,
+        mode = 'c',
+        buffer = true,
+      }
+    end,
+  })
 end
 
 return NVEditing
