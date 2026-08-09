fe7636713a1a4b880869b9274a8a2eaea49f404e
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-09 01:07:11 -0500

    editing keymap cleanup

--- lua/editor/editing.lua
+++ lua/editor/editing.lua
@@ -1,5 +1,7 @@
 NVEditing = {}
 
+local fn = {}
+
 function NVEditing.esc()
   if NVLspPopup.ensure_hidden() then
     return
@@ -9,64 +11,119 @@ function NVEditing.esc()
   vim.cmd 'silent noh'
 end
 
+function fn.save()
+  NVEditing.esc()
+  vim.cmd 'silent w'
+end
+
+function fn.save_all()
+  fn.save()
+  vim.cmd 'silent! wa'
+end
+
+function fn.paste()
+  local mode = vim.fn.mode()
+
+  if mode == 'i' or mode == 'c' then
+    local paste = vim.o.paste
+    local fopts = vim.o.formatoptions
+
+    vim.o.paste = true
+    vim.o.formatoptions = fopts:gsub('[crota]', '')
+
+    NVKeys.send('<C-r>+', { mode = 'n' })
+
+    vim.defer_fn(function()
+      vim.o.paste = paste
+      vim.o.formatoptions = fopts
+    end, 10)
+  else
+    log.error 'Unexpected mode'
+  end
+end
+
+function fn.jump_to_end_of_word()
+  require('spider').motion 'e'
+
+  local current_col = vim.fn.col '.'
+  local end_col = vim.fn.col '$'
+
+  if current_col == end_col - 1 then
+    NVKeys.send('<Esc>A', { mode = 'n' })
+  elseif current_col ~= end_col then
+    vim.cmd 'normal! l'
+  end
+end
+
+function fn.toggle_tabs()
+  local current = vim.opt.listchars:get()
+  if current.tab == '» ' then
+    vim.notify('Disabled **Tabs**', vim.log.levels.WARN, { title = 'Tabs' })
+    vim.opt.listchars:append {
+      tab = '  ',
+    }
+  else
+    vim.notify('Enabled **Tabs**', vim.log.levels.INFO, { title = 'Tabs' })
+    vim.opt.listchars:append {
+      tab = '» ',
+    }
+  end
+end
+
 function NVEditing.keymaps()
-  -- TODO!: this might be fine actually
   K.map { '<Esc>', 'Drop all the noise and Esc', '<Cmd>lua NVEditing.esc()<CR><Esc>', mode = 'n', silent = false }
 
-  -- TODO: new keymap for these two items
   K.map {
-    '<M-S-s>',
-    'Save all files',
-    function()
-      NVEditing.esc()
-      vim.cmd 'silent w'
-      vim.cmd 'silent! wa'
-    end,
+    '<A-s>',
+    'Save file',
+    fn.save,
     mode = 'n',
   }
   K.map {
-    '<M-S-s>',
+    '<A-s>',
+    'Save file',
+    '<Cmd>lua NVEditing.esc()<CR><Esc><Cmd>silent w<CR>',
+    mode = { 'i', 'v' },
+  }
+  K.map {
+    '<A-S-s>',
     'Save all files',
-    '<Esc><Cmd>silent w<CR><Cmd>silent! wa<CR>',
+    fn.save_all,
+    mode = 'n',
+  }
+  K.map {
+    '<A-S-s>',
+    'Save all files',
+    '<Cmd>lua NVEditing.esc()<CR><Esc><Cmd>silent w<CR><Cmd>silent! wa<CR>',
     mode = { 'i', 'v' },
   }
 
   K.map { 'J', 'Join lines and keep cursor position', 'mzJ`z', mode = 'n' }
   K.map { 'U', 'Redo', '<C-r>', mode = 'n' }
-  K.map { 'x', "Don't yank on delete", '"_x', mode = { 'n', 'x', 's' } }
-  K.map { 'X', "Don't yank on delete", '"_X', mode = { 'n', 'x', 's' } }
 
   K.map { '<M-o>', 'New line below', 'o<Esc>', mode = 'n' }
   K.map { '<M-S-o>', 'New line above', 'O<Esc>', mode = 'n' }
 
-  -- TODO?: revisit?
-  -- K.map { '<leader>yd', 'Duplicate line', 'yyp', mode = 'n' }
-  -- K.map {
-  --   '<leader>yd',
-  --   'Duplicate selection',
-  --   [["yy']y"ypgv]],
-  --   mode = 'v',
-  -- }
+  K.map { '<C-S-c>', 'Copy selected text', [["+y]], mode = 'v' } -- TODO: make into omarchy universal copy Ctrl+Insert
+  K.map { '<C-S-v>', 'Paste text', 'P', mode = { 'n', 'v' } } -- TODO: make into omarchy universal paste Shift+Insert
+  K.map { '<C-S-v>', 'Paste text', fn.paste, mode = { 'i', 'c' } }
+  -- TODO: also make a keymap for cut?
 
-  -- TODO!: revisit
   K.map {
-    '<C-S-v>',
-    'Paste without auto-formatting (insert mode)',
+    'p',
+    "Don't replace clipboard content when pasting",
     function()
-      local saved_paste = vim.o.paste
-      local saved_fo = vim.o.formatoptions
-      vim.o.paste = true
-      vim.o.formatoptions = saved_fo:gsub('[crota]', '')
-      NVKeys.send('<C-r>+', { mode = 'n' })
-      vim.defer_fn(function()
-        vim.o.paste = saved_paste
-        vim.o.formatoptions = saved_fo
-      end, 10)
+      return 'pgv"' .. vim.v.register .. 'ygv'
     end,
-    mode = 'i',
+    mode = 'v',
+    expr = true,
   }
+  -- K.map { '<leader>p', 'Paste without yanking', [["_dP]], mode = { 'x', 'v', 's' } }
 
-  K.map { '<leader>p', 'Paste without yanking', [["_dP]], mode = { 'x', 'v', 's' } }
+  K.map { 'x', "Don't replace clipboard content when deleting", [["_x]], mode = { 'n', 'v' } }
+  K.map { 'X', "Don't replace clipboard content when deleting", [["_X]], mode = { 'n', 'v' } }
+  K.map { 's', "Don't replace clipboard content when inserting", [["xs]], mode = 'v' }
+  K.map { 'c', "Don't replace clipboard content when changing", [["xc]], mode = { 'n', 'v' } }
 
   -- TODO: make new Alt-q, Alt-Q, etc. keymaps for closing and restarting and stuff
   -- make an option to exit and wipe session?
@@ -81,26 +138,8 @@ function NVEditing.keymaps()
     end,
     mode = 'n',
   }
-  -- TODO!: make <leader>qq save session if not doing that?
 
-  -- TODO: make a more general toggle for listchars? to display whitespace as well for diffing purposes?
-  -- TODO: make a general toggle message api/helper?
-  local function toggle_tabs()
-    local current = vim.opt.listchars:get()
-    if current.tab == '» ' then
-      vim.notify('Disabled **Tabs**', vim.log.levels.WARN, { title = 'Tabs' })
-      vim.opt.listchars:append {
-        tab = '  ',
-      }
-    else
-      vim.notify('Enabled **Tabs**', vim.log.levels.INFO, { title = 'Tabs' })
-      vim.opt.listchars:append {
-        tab = '» ',
-      }
-    end
-  end
-
-  K.map { '<leader>u<tab>', 'Toggle tab characters', toggle_tabs, mode = 'n' }
+  K.map { '<leader>u<tab>', 'Toggle tab characters', fn.toggle_tabs, mode = 'n' }
 end
 
 return NVEditing
