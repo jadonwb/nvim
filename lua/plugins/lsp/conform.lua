0091f394037806325fd02e71d037407e9830f804
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-09-10 15:11:59 -0500

    format all markdown on save

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -6,6 +6,20 @@ return {
       default_format_opts = {
         lsp_format = 'fallback',
       },
+      formatters = {
+        prettier = {
+          condition = function()
+            return true
+          end,
+          require_cwd = false,
+          prepend_args = {
+            '--config',
+            vim.fn.expand '~/.prettierrc.yaml',
+            '--config-precedence',
+            'prefer-file',
+          },
+        },
+      },
       formatters_by_ft = {
         ['markdown'] = { 'prettier' },
         ['markdown.mdx'] = { 'prettier' },
